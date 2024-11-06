5b66be7e92879377a2675105cad64cb0a5bcf27d
User <User@DESKTOP-0TCDSR6>
2024-11-06 16:56:22 +0300

    Library2 start

---
+++ Introduction/Library2/MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Library2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBoxQuery = new System.Windows.Forms.TextBox();
+			this.buttonExecute = new System.Windows.Forms.Button();
+			this.dataGridView = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBoxQuery
+			// 
+			this.textBoxQuery.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxQuery.Location = new System.Drawing.Point(13, 13);
+			this.textBoxQuery.Name = "textBoxQuery";
+			this.textBoxQuery.Size = new System.Drawing.Size(343, 20);
+			this.textBoxQuery.TabIndex = 0;
+			// 
+			// buttonExecute
+			// 
+			this.buttonExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonExecute.Location = new System.Drawing.Point(374, 13);
+			this.buttonExecute.Name = "buttonExecute";
+			this.buttonExecute.Size = new System.Drawing.Size(75, 20);
+			this.buttonExecute.TabIndex = 1;
+			this.buttonExecute.Text = "Execute";
+			this.buttonExecute.UseVisualStyleBackColor = true;
+			this.buttonExecute.Click += new System.EventHandler(this.buttonExecute_Click);
+			// 
+			// dataGridView
+			// 
+			this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView.Location = new System.Drawing.Point(13, 40);
+			this.dataGridView.Name = "dataGridView";
+			this.dataGridView.Size = new System.Drawing.Size(436, 181);
+			this.dataGridView.TabIndex = 2;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(461, 233);
+			this.Controls.Add(this.dataGridView);
+			this.Controls.Add(this.buttonExecute);
+			this.Controls.Add(this.textBoxQuery);
+			this.Name = "MainForm";
+			this.Text = "Library2";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxQuery;
+		private System.Windows.Forms.Button buttonExecute;
+		private System.Windows.Forms.DataGridView dataGridView;
+	}
+}
+
---
+++ Introduction/Library2/MainForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Library2
+{
+	public partial class MainForm : Form
+	{
+		readonly string connectionString = 
+			ConfigurationManager.ConnectionStrings["ConnectionString_2_Library_PD_311"].ConnectionString;
+		SqlConnection connection;
+		public MainForm()
+		{
+			connection = new SqlConnection(connectionString);
+			InitializeComponent();
+		}
+
+		private void buttonExecute_Click(object sender, EventArgs e)
+		{
+			string cmd = textBoxQuery.Text;
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			DataTable table = new DataTable();
+			SqlDataReader reader = command.ExecuteReader();
+			for (int i = 0; i < reader.FieldCount; i++)
+				table.Columns.Add(reader.GetName(i));
+
+			while (reader.Read())
+			{
+				DataRow row = table.NewRow();
+				for (int i = 0; i < reader.FieldCount; i++)
+					row[i] = reader[i];
+				table.Rows.Add(row);
+			}
+			reader.Close();
+			connection.Close();
+			dataGridView.DataSource = table;
+		}
+	}
+}
